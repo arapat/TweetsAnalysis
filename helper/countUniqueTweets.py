be5e39016389d349959e583df6a43b7ee6de5bbb
Arapat Alafate <arapat.mail@gmail.com>
2013-12-22 07:21:16 -0800

    optimized countUniqueTweets's performance

--- helper/countUniqueTweets.py
+++ helper/countUniqueTweets.py
@@ -3,18 +3,9 @@ import json
 from operator import add
 from pyspark import SparkContext
 
-sc = SparkContext("ion-21-14.local:7077", "Count Unique Tweets", pyFiles=['countUniqueTweets.py'])
-
-dir_path = '/oasis/projects/nsf/csd181/arapat/project/twitter/raw/comb_election1'
-all_tweets = gen_rdd(os.path.join(dir_path, 't01'))
-for fnum in range(2, 71):
-    file_name = os.path.join(dir_path, 't' + '%02d' % fnum)
-    all_tweets = all_tweets.join(gen_rdd(file_name))
-print "Total unique tweets: ", all_tweets.count()-1
-
 
 def gen_rdd(tweets_file):
-    return sc.textFile(file_name).map(process_tweet).distinct()
+    return sc.textFile(tweets_file).map(process_tweet)
 
 def process_tweet(raw_tweet):
     try:
@@ -22,4 +13,14 @@ def process_tweet(raw_tweet):
         return tweet['id']
     except:
         return 0
+
+if __name__ == "__main__":
+  sc = SparkContext("spark://ion-21-14.sdsc.edu:7077", "CountUniqueTweets", pyFiles=['countUniqueTweets.py'])
+
+  dir_path = 'twitter/election1'
+  all_tweets = gen_rdd(os.path.join(dir_path, 't01'))
+  for fnum in range(2, 71):
+      file_name = os.path.join(dir_path, 't' + '%02d' % fnum)
+      all_tweets = all_tweets.union(gen_rdd(file_name))
+  print "Total unique tweets: ", all_tweets.distinct().count()-1
 
