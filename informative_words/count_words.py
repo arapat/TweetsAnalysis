607ec64e818b205ade7a7799b14e81067b52f7e0
Arapat Alafate <arapat@arap.at>
2014-01-29 18:24:36 +0800

    removed CountWords class

--- informative_words/count_words.py
+++ informative_words/count_words.py
@@ -5,35 +5,34 @@
 import json
 from pyspark import SparkContext
 
-class CountWords:
-    sc = SparkContext("spark://ion-21-14.sdsc.edu:7077", "CountUniqueTweets", pyFiles=['countUniqueTweets.py'])
 
-    def gen_rdd(self, tweets_file):
-        return self.sc.textFile(tweets_file).map(self.process_tweet)
+def gen_rdd(tweets_file):
+    return sc.textFile(tweets_file).map(process_tweet)
 
-    def process_tweet(self, raw_tweet):
-        try:
-            tweet = json.loads(raw_tweet)
-            return tweet['text'].split()
-        except:
-            return 0
+def process_tweet(raw_tweet):
+    try:
+        tweet = json.loads(raw_tweet)
+        return tweet['text'].split()
+    except:
+        return 0
 
-    def count_unique_words(self, files):
-        if len(files):
-            return 0
+def count_unique_words(files):
+    if len(files) == 0:
+        return 0
 
-        all_words = None
-        for file_name in files:
-            words = self.gen_rdd(file_name)
-            if all_words:
-                all_words = all_words.union(words)
-            else:
-                all_words = words
+    all_words = None
+    for file_name in files:
+        words = gen_rdd(file_name)
+        if all_words:
+            all_words = all_words.union(words)
+        else:
+            all_words = words
 
-        return all_words.distinct().count()
+    return all_words.distinct().count()
 
 
 if __name__ == '__main__':
+    sc = SparkContext("spark://ion-21-14.sdsc.edu:7077", "CountWords", pyFiles=['countWords.py'])
     dir_path = '/user/arapat/twitter/'
     files = [dir_path + 't%02d' % k for k in range(1, 71)] + [dir_path + 'u%02d' % k for k in range(1,86)]
 
