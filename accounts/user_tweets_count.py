54ca6146f8478cff242d18465a568ca0d010e2ec
Arapat Alafate <arapat.mail@gmail.com>
2014-01-30 02:43:45 -0800

    obtained user tweets counts

--- accounts/user_tweets_count.py
+++ accounts/user_tweets_count.py
@@ -6,7 +6,7 @@ from pyspark import SparkContext
 
 
 def gen_rdd(tweets_file):
-    return sc.textFile(tweets_file).flatMap(process_tweet)
+    return sc.textFile(tweets_file).map(process_tweet)
 
 
 def process_tweet(raw_tweet):
@@ -14,7 +14,7 @@ def process_tweet(raw_tweet):
         tweet = json.loads(raw_tweet)
         return (tweet["user"]["id"], 1)
     except:
-        return []
+        return (0, 1)
 
 
 def user_tweets_counts(files):
@@ -29,24 +29,30 @@ def user_tweets_counts(files):
         else:
             all_users = users
 
-    all_users = all_users.reduceByKey(add) \
-            .map(lambda (a, b): (b, 1)) \
-            .reduceByKey(add) \
-            .cache()
+    all_users = all_users.filter(lambda (a, b): a == 0).reduceByKey(add).collect()
+    print all_users
+    return 0
+    #all_users = all_users.reduceByKey(add) \
+    #        .map(lambda (a, b): (b, 1)) \
+    #        .reduceByKey(add) \
+    #        .sortByKey(ascending=False)
 
-    print "Top 1000 active users:"
-    all_users.sortByKey(ascending=False).take(1000)
-    print "Top 1000 silence users:"
-    all_users.sortByKey(ascending=True).take(1000)
+    #ul = all_users.collect()
+    #print "posted_tweets_count, users_count"
+    #for (t, u) in ul:
+    #    print t, u
 
-    return all_users.count()
+    #total = 0
+    #for (t, u) in ul:
+    #    total += u
+    #return total
 
 
 if __name__ == '__main__':
     sc = SparkContext("spark://ion-21-14.sdsc.edu:7077", "UserTweetsCount", pyFiles=['user_tweets_count.py'])
     dir_path = '/user/arapat/twitter/'
-    # files = [dir_path + 't%02d' % k for k in range(1, 71)] + [dir_path + 'u%02d' % k for k in range(1,86)]
-    files = [dir_path + 't01', dir_path + 't02']
+    files = [dir_path + 't%02d' % k for k in range(1, 71)] + [dir_path + 'u%02d' % k for k in range(1,86)]
+    # files = [dir_path + 't01', dir_path + 't02']
 
-    print "Total users:", word_counts(files)
+    print "Total users:", user_tweets_counts(files)
 
