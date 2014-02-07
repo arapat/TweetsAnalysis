ae618d31b4d3f2b35c223647ea09a79f95e1d220
Arapat Alafate <arapat@arap.at>
2014-02-08 01:07:40 +0800

    implemented initial version of user clustering

---
+++ users/clusteringUsers.py
@@ -0,0 +1,171 @@
+# Note that the add operation in K-means algorithm is defined WITHOUT factors
+
+import os
+import json
+from operator import add
+from pyspark import SparkContext
+
+
+def gen_rdd(tweets_file):
+    return sc.textFile(tweets_file).map(process_tweet)
+
+
+def process_tweet(raw_data):
+    try:
+        raw_data = raw_data.split('\t')
+        raw_json = json.loads(raw_data[3])
+        uid = raw_json['user']['id']
+        text = [w.strip() for w in raw_data[0].split()]
+        return (uid, (1, text))
+    except:
+        return (0, (0, set([])))
+
+
+def clustering_user(files):
+
+    def diff(dist1, dist2):
+        vec1 = dist1[2]
+        vec2 = dist2[2]
+        len1 = len(vec1)
+        len2 = len(vec2)
+        result = 0.0
+        itr1 = 0
+        itr2 = 0
+        while itr1 < len1 and itr2 < len2:
+            if vec1[itr1][0] < vec2[itr2][0]:
+                result += vec1[itr1][1] * vec1[itr1][1]
+                itr1 += 1
+            elif vec1[itr1][0] > vec2[itr2][0]:
+                result += vec2[itr2][1] * vec2[itr2][1]
+                itr2 += 1
+            else:
+                result += (vec1[itr1][1] - vec2[itr2][1]) ** 2
+                itr1 += 1
+                itr2 += 1
+        while itr1 < len1:
+            result += vec1[itr1][1] * vec1[itr1][1]
+            itr1 += 1
+        while itr2 < len2:
+            result += vec2[itr2][1] * vec2[itr2][1]
+            itr2 += 1
+        return result
+
+
+    def union(dist1, dist2):
+        vec1 = dist1[2]
+        vec2 = dist2[2]
+        len1 = len(vec1)
+        len2 = len(vec2)
+        result = []
+        itr1 = 0
+        itr2 = 0
+        while itr1 < len1 and itr2 < len2:
+            if vec1[itr1][0] < vec2[itr2][0]:
+                result.append(vec1[itr1])
+                itr1 += 1
+            elif vec1[itr1][0] > vec2[itr2][0]:
+                result.append(vec2[itr2])
+                itr2 += 1
+            else:
+                result.append((vec1[itr1][0], vec1[itr1][1] + vec2[itr2][1]))
+                itr1 += 1
+                itr2 += 1
+        while itr1 < len1:
+            result.append(vec1[itr1])
+            itr1 += 1
+        while itr2 < len2:
+            result.append(vec2[itr2])
+            itr2 += 1
+        return [0, 0, result]
+    
+
+    def average(dist, k):
+        result = dist[2]
+        for itr in range(len(result)):
+            result[itr][1] = float(result[itr][1]) / dist
+        return [dist[0], dist[1], result]
+
+
+    def normalize(user):
+        uid = user[0]
+        counter = float(user[1][0])
+        tokens = sorted(user[1][1])
+
+        vec = []
+        tp = (token[0], 1)
+        for i in range(1,len(tokens)):
+            if tp[0] == tokens[i]:
+                tp[1] += 1
+            else:
+                vec.append(tp)
+                tp = (token[i], 1)
+        vec.append(tp)
+        vec = map(lambda (token, weight): (token, weight / counter), vec)
+        return (uid, counter, vec)
+
+
+    def closestPoint(p, centers):
+        bestIndex = 0
+        closest = float("+inf")
+        for i in range(len(centers)):
+            tempDist = diff(p, centers[i]) # np.sum((p - centers[i]) ** 2)
+            if tempDist < closest:
+                closest = tempDist
+                bestIndex = i
+        return bestIndex
+
+
+    if len(files) == 0:
+        return 0
+
+    # Ignore users that have fewer than MIN_POST tweets
+    MIN_POST = 7
+
+    # For each account, put all the tokens of its tweets into one vector
+    all_users = None
+    for file_name in files:
+        users = process_tweet(file_name)
+        if all_users:
+            all_word_counts = all_word_counts.union(word_counts)
+        else:
+            all_users = users
+
+    # Normalize the vector
+    all_users.reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) \
+            .filter(lambda (uid, (counter, tokens)): counter >= MIN_POST) \
+            .map(normalize)
+
+    # Clustering (k-means)
+    K = 10
+    CONVERGE = 0.001
+
+    kPoints = data.takeSample(True, K, 1)
+    temp_dist = 1.0
+
+    while temp_dist > CONVERGE:
+        closest = all_users.map(
+            lambda p : (closestPoint(p, kPoints), (p, 1)))
+        pointStats = closest.reduceByKey(
+            lambda (p1, counter1), (p2, counter2): (union(p1, p2), counter1 + counter2))
+        newPoints = pointStats.map(
+            lambda (cid, (ctr, counter)): (cid, average(ctr, counter))).collect()
+
+        tempDist = sum(diff(kPoints[cid], ctr) for (cid, ctr) in newPoints)
+
+        for (x, y) in newPoints:
+            kPoints[x] = y
+
+    print "Final centers:"
+    for point in kPoints:
+        print point[2]
+
+    return all_users.count()
+    
+
+if __name__ == "__main__":
+    sc = SparkContext("spark://ion-21-14.sdsc.edu:7077", "ClusteringUsers", pyFiles=['clusteringUsers.py'])
+
+    dir_path = '/user/arapat/twitter-tag/'
+    files = [dir_path + 't%02d' % k for k in range(1, 71)] + [dir_path + 'u%02d' % k for k in range(1,86)]
+    print clustering_user(files)
+
